ab549a2f0927ca0bf263754584825a3829315c3f
Elena Hayrova <hayrova.el@icloud.com>
2024-07-14 16:17:39 +0300

    done

--- Navigation3 /Navigation1/FeedViewController.swift
+++ Navigation3 /Navigation1/FeedViewController.swift
@@ -9,14 +9,32 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    lazy var button: UIButton = {
-        let button = UIButton(frame: CGRectMake(50, 250, 50, 50))
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var button1: UIButton = {
+        let button = UIButton()
         button.backgroundColor = .blue
         button.layer.cornerRadius = 12
-        button.setTitle("пост", for: .normal)
-        button.setTitleColor(.red, for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = true
+        button.setTitle("Пост 1", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var button2: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .green
+        button.layer.cornerRadius = 12
+        button.setTitle("Пост 2", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
         return button
     }()
     
@@ -25,7 +43,22 @@ class FeedViewController: UIViewController {
         
         view.backgroundColor = .gray
         
-        self.view.addSubview(button)
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(button1)
+        stackView.addArrangedSubview(button2)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button1.widthAnchor.constraint(equalToConstant: 200),
+            button1.heightAnchor.constraint(equalToConstant: 50),
+            button2.widthAnchor.constraint(equalToConstant: 200),
+            button2.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     struct Post {
@@ -33,11 +66,9 @@ class FeedViewController: UIViewController {
     }
     var post = Post(title: "Пост")
     
-    @objc  func buttonAction(_ sender: UIButton) {
+    @objc func buttonAction(_ sender: UIButton) {
         let postViewController = PostViewController()
-        postViewController.post = Post(title: "Пост")
+        postViewController.post = Post(title: sender.currentTitle ?? "Пост")
         self.navigationController?.pushViewController(postViewController, animated: true)
     }
-    
-    
 }
--- Navigation3 /Navigation1/Profile/ProfileViewController.swift
+++ Navigation3 /Navigation1/Profile/ProfileViewController.swift
@@ -4,24 +4,39 @@
 //
 //  Created by Елена Хайрова on 29.05.2024.
 //
-
 import UIKit
 
 class ProfileViewController: UIViewController {
     var profileHeaderView: ProfileHeaderView!
+    var newButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
 
-        
         profileHeaderView = ProfileHeaderView()
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(profileHeaderView)
+        
+        newButton = UIButton(type: .system)
+        newButton.setTitle("кнопка", for: .normal)
+        newButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(newButton)
+        
+        setupConstraints()
     }
 
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-        profileHeaderView.frame = self.view.frame
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            profileHeaderView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            profileHeaderView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            
+            newButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            newButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            newButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
