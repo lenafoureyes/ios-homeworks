45967e641be8f274b0d22223aa611ecbc214bc6d
Elena Hayrova <hayrova.el@icloud.com>
2024-07-25 21:25:04 +0300

    add

---
+++ задание 4/Navigation1/Profile/ProfileHeaderView.swift
@@ -0,0 +1,103 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation1
+//
+//  Created by Елена Хайрова on 17.06.2024.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+    let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "cat")
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = UIColor(white: 242.0/255.0, alpha: 1.0).cgColor
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 75
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .black
+        label.text = "Cat"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .darkGray
+        label.text = "mew"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+   private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Нажми меня", for: .normal)
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.backgroundColor = .blue
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        addSubview(avatarImageView)
+        addSubview(nameLabel)
+        addSubview(descriptionLabel)
+        addSubview(button)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 150),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 150),
+            
+            nameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 27),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 34),
+            nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 18),
+            descriptionLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 34),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            
+            button.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc func buttonPressed() {
+        if let text = descriptionLabel.text {
+            print(text)
+        }
+    }
+}
